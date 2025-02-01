bdaea2f13d9cbc0259a4193a05764e35e452e76a
graphFatigue <foxmary007@gmail.com>
2025-02-01 12:22:37 +0200

    small changes blob

--- backend/TravelTales/TravelTales.Domain/Entities/Blob.cs
+++ backend/TravelTales/TravelTales.Domain/Entities/Blob.cs
@@ -2,5 +2,9 @@
 {
     public class Blob
     {
+        public string? Uri { get; set; }
+        public string? Name { get; set; }
+        public string? ContentType { get; set; }
+        public Stream? Content { get; set; }
     }
 }
--- backend/TravelTales/TravelTales.Domain/Entities/Blogger.cs
+++ backend/TravelTales/TravelTales.Domain/Entities/Blogger.cs
@@ -6,11 +6,12 @@ namespace TravelTales.Domain.Entities
     public class Blogger : IEntityBase<long>
     {
         public long Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public Sex Sex { get; set; }
-        public string Bio { get; set; }
+        public string? Bio { get; set; }
+        public string? Image { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
         public ICollection<Post>? Posts { get; }

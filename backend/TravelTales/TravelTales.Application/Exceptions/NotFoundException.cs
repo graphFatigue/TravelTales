e43cd28a3fc30d3b94aa1434de1e8fe7add745a4
graphFatigue <foxmary007@gmail.com>
2025-02-02 12:29:42 +0200

    added exceptions

---
+++ backend/TravelTales/TravelTales.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace TravelTales.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        : base("The entity isn't found")
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}

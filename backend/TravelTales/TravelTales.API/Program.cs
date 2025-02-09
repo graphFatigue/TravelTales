ca055a0f3fc5373153e5c7c0e5d965618e9fae6c
graphFatigue <foxmary007@gmail.com>
2025-02-09 12:51:29 +0200

    updated Program.cs

--- backend/TravelTales/TravelTales.API/Program.cs
+++ backend/TravelTales/TravelTales.API/Program.cs
@@ -1,15 +1,71 @@
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using TravelTales.API.Hubs;
+using TravelTales.API.Middleware;
+using TravelTales.Application;
+using TravelTales.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer ...\"",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                },
+            },
+            Array.Empty<string>()
+        },
+    });
+});
+
+builder.Services.AddCors(options => options.AddPolicy(
+    "Frontend",
+    policy => policy
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("https://localhost:4200")
+        .AllowCredentials()));
+
+builder.Services.AddAuthorization();
+builder.Services.ConfigureApplicationLayerDependencies(builder.Configuration);
+builder.Services.ConfigurePersistenceLayerDependencies(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    await TravelTales.Application.DependencyRegistar.CreateUserRolesAsync(serviceProvider, builder.Configuration);
+}
+
+app.UseExceptionHandlingMiddleware();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +73,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
+app.UseCors("Frontend");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<LikesHub>("hubs/likes");
 
 app.Run();

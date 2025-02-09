0531d944a56d521874d75a2dc75ae8088a9596cb
graphFatigue <foxmary007@gmail.com>
2025-02-09 12:08:22 +0200

    added likes hub

---
+++ backend/TravelTales/TravelTales.API/Hubs/LikesHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TravelTales.Application.DTOs.PostLike;
+using TravelTales.Application.Interfaces;
+
+namespace TravelTales.API.Hubs
+{
+    public class LikesHub : Hub
+    {
+        private readonly ILikeService likeService;
+
+        public LikesHub(ILikeService likeService)
+        {
+            this.likeService = likeService;
+        }
+
+        public async Task SetLike(CreatePostLikeDto createPostLikeDto)
+        {
+            try
+            {
+                await this.likeService.AddLikeAsync(createPostLikeDto);
+                bool isLiked = await this.likeService.IsLikedAsync(createPostLikeDto.PostId, createPostLikeDto.BloggerId);
+                int numOfLikes = await this.likeService.CountLikesByPostIdAsync(createPostLikeDto.PostId);
+                await this.Clients.All.SendAsync("ReceiveMessage", numOfLikes, isLiked, createPostLikeDto.PostId, createPostLikeDto.BloggerId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in SetLike: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}

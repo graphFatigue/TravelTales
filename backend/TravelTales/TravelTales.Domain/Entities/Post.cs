ff79bf46e90fe3962c148dbad779841aa28a9f4d
graphFatigue <foxmary007@gmail.com>
2025-02-15 12:24:26 +0200

    post updates

--- backend/TravelTales/TravelTales.Domain/Entities/Post.cs
+++ backend/TravelTales/TravelTales.Domain/Entities/Post.cs
@@ -9,6 +9,7 @@ namespace TravelTales.Domain.Entities
         public string Content { get; set; }
         public long BloggerId { get; set; }
         public Blogger Blogger { get; set; }
+        public ICollection<Attachment>? Attachments { get; }
         public ICollection<PostLike>? Likes { get; }
         public ICollection<Comment>? Comments { get; }
         public DateTime? CreatedAt { get; set; }

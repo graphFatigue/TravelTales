01713485bdc85578d3fc2a6fc1e2d4bce1251dd1
Iryna <bibik.iryna004@gmail.com>
2025-05-04 11:34:38 +0300

    upd

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,11 +1,19 @@
 import Navbar from './Navbar';
 import MenuBar from './MenuBar';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
-}) {
+	}) {
+	
+	const user = JSON.parse(localStorage.getItem('user') || '{}');
+
+	if (!user) {
+		redirect('/login');
+	}
+
 	return (
 		<div className="flex min-h-screen flex-col">
 			<Navbar />
--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,13 +1,4 @@
-'use client';
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-	const user = JSON.parse(localStorage.getItem('user') || '{}');
-
-	if (!user) {
-		redirect('/login');
-	}
-
 	return (
 		// <main className="h-[200vh] w-full bg-red-50">
 		// 	<div className="w-full">

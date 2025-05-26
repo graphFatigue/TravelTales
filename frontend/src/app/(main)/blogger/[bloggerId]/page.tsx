d8494c3d61d4559c22f83cf88aa7148ba17bcb2a
Iryna <bibik.iryna004@gmail.com>
2025-05-27 01:13:13 +0300

    update for next build

--- frontend/src/app/(main)/blogger/[bloggerId]/page.tsx
+++ frontend/src/app/(main)/blogger/[bloggerId]/page.tsx
@@ -4,7 +4,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import ProfilePageClient from './ProfilePageClient';
 
-export const getBlogger = async (bloggerId: string, token?: string) => {
+const getBlogger = async (bloggerId: string, token?: string) => {
 	const headers = token ? { Authorization: `Bearer ${token}` } : {};
 	const { data: blogger } = await api.get(`/api/Blogger/${bloggerId}`, {
 		headers,

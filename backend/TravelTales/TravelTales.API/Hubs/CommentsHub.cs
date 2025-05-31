c173c53ee1129abb8e173fc260bc9321a15e8b5a
graphFatigue <foxmary007@gmail.com>
2025-05-31 16:38:39 +0300

    upd

--- backend/TravelTales/TravelTales.API/Hubs/CommentsHub.cs
+++ backend/TravelTales/TravelTales.API/Hubs/CommentsHub.cs
@@ -28,7 +28,7 @@ namespace TravelTales.API.Hubs
             {
                 await notificationsHub.Clients
                     .Group(createdComment.PostAuthorBloggerId.ToString())
-                    .SendAsync("ReceiveNotification", "New comment received!", createdComment, notification);
+                    .SendAsync("ReceiveNotification", "New comment received!", notification);
             }
 
             await Clients.Group(commentDto.PostId.ToString())

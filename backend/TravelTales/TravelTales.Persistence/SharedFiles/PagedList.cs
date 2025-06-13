1580d40e20382568f89e21d7a0ce6e97f2b36aa1
graphFatigue <foxmary007@gmail.com>
2025-06-13 14:04:01 +0300

    upd

--- backend/TravelTales/TravelTales.Persistence/SharedFiles/PagedList.cs
+++ backend/TravelTales/TravelTales.Persistence/SharedFiles/PagedList.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using Sieve.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TravelTales.Persistence.SharedFiles
 {
